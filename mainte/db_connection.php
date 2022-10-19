de6db609197f8d3af4ebdc0261abb67d08684d65
DJ-phi <94509379+DJ-phi@users.noreply.github.com>
2022-10-19 16:35:22 +0900

    PDO 接続コード

---
+++ mainte/db_connection.php
@@ -0,0 +1,8 @@
+<?php
+const DB_HOST = "mysql:dbname=udemy_php;host=127.0.0.1;charset=utf8";
+const DB_USER = "php_user";
+const DB_PASSWORD = "1234";
+$pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD);
+
+
+?>
